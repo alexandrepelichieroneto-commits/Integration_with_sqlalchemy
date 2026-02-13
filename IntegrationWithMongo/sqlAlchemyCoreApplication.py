1d1f31bcf5e4dbcecfeebc34071325ce572a068b
Alexandre Pelichiero <alexandrepelichiero@outlook.com>
2026-02-12 23:42:21 -0300

    Finalização do Curso/Projeto

---
+++ IntegrationWithMongo/sqlAlchemyCoreApplication.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+engine = create_engine('sqlite:///:memory')
+
+metadata_obj = MetaData(schema='teste')
+user = Table(
+    'user',
+    metadata_obj,
+    Column('user_id', Integer, primary_key=True),
+    Column('user_name', String(40), nullable=False),
+    Column('email_address', String(60)),
+    Column('nickname', String(50), nullable=False)
+)
+
+user_prefs = Table(
+    'user_prefs',
+    metadata_obj,
+    Column('pref_id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey("user.user_id"), nullable=False),
+    Column('pref_name', String(40), nullable=False),
+    Column('pref_value', String(100)),
+)
+
+print("\nInformações tabela users")
+print(user_prefs.primary_key)
+print(user_prefs.constraints)
+
+print(metadata_obj.tables)
+
+for table in metadata_obj.sorted_tables:
+    print(table)
+
+
+
+metadata_bd_obj = MetaData(schema='bank')
+financial_info = Table(
+    'financial_info',
+    metadata_bd_obj,
+    Column('id', Integer, primary_key=True),
+    Column('value', String(100), nullable=False),
+)
+
+print("\nInformações tabela financial_info")
+print(financial_info.primary_key)
+print(financial_info.constraints)
+
